fbe1471e999255b7135ec83975bc615170da235c
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-05-11 00:32:36 +0600

    query get by id api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -31,10 +31,20 @@ async function run() {
         // await client.connect();
         const queriesCollection = client.db('queryNest').collection('queries');
 
+        // get all queries
         app.get('/queries', async (req, res) => {
             const result = await queriesCollection.find().toArray();
             res.send(result);
         })
+
+        //get a single query
+        app.get('/query/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await queriesCollection.findOne(query);
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
